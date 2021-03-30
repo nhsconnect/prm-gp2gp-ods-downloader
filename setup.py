8c02fdaf01c2d752a42666128410a3d8fc010e62
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-03-30 10:18:58 +0100

    PRMT-1435 [KV/EA] Add packages, setup, and etc

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gp2gp-ods-downloader",
+    version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "python-dateutil~=2.8",
+        "requests~=2.2",
+        "boto3~=1.12",
+        "PyArrow~=3.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ods-portal-pipeline=prmods.pipeline.ods_downloader.main:main",
+        ]
+    },
+)
