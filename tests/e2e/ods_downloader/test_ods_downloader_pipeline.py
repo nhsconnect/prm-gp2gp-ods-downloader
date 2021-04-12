2f4906db852d073524445f3986fc3fb31bc8a223
James Green <james.green23@nhs.net>
2021-04-12 17:13:01 +0100

    PRMT-1435 Run e2e tests against mock ods portal

--- tests/e2e/ods_downloader/test_ods_downloader_pipeline.py
+++ tests/e2e/ods_downloader/test_ods_downloader_pipeline.py
@@ -1,6 +1,8 @@
-import gzip
-import os
-import shutil
+import json
+from http.server import BaseHTTPRequestHandler
+from io import BytesIO
+from urllib.parse import parse_qs
+
 import boto3
 
 from os import environ
@@ -11,6 +13,56 @@ from werkzeug.serving import make_server
 
 from prmods.pipeline.ods_downloader.config import OdsPortalConfig
 from prmods.pipeline.ods_downloader.main import main
+from tests.builders.file import build_gzip_csv
+
+FAKE_ODS_HOST = "127.0.0.1"
+FAKE_ODS_PORT = 9000
+FAKE_ODS_PORTAL_URL = f"http://{FAKE_ODS_HOST}:{FAKE_ODS_PORT}"
+
+FAKE_S3_HOST = "127.0.0.1"
+FAKE_S3_PORT = 8887
+FAKE_S3_URL = f"http://{FAKE_S3_HOST}:{FAKE_S3_PORT}"
+FAKE_S3_ACCESS_KEY = "testing"
+FAKE_S3_SECRET_KEY = "testing"
+FAKE_S3_REGION = "us-west-1"
+
+INPUT_ROWS = [
+    ["000011357014", "A12345", "Test GP", "Supplier", "system", "Practice", "HT87 1PQ"],
+    ["000022357014", "B12345", "Test GP 2", "Supplier", "System", "Practice", "HY87 1PQ"],
+    ["000033357014", "C12345", "Test GP 3", "Supplier", "System", "Practice", "HP87 1PQ"],
+    ["123433357014", "C12345", "Test GP 3", "Supplier", "Other System", "Practice", "HP87 1PQ"],
+    ["000044357014", "D12345", "Test GP 4", "Supplier", "System", "Practice", "HQ87 1PQ"],
+    ["000055357014", "E12345", "Test GP 5", "Supplier", "System", "Practice", "HZ87 1PQ"],
+]
+
+INPUT_ASID_CSV = BytesIO(
+    build_gzip_csv(
+        header=["ASID", "NACS", "OrgName", "MName", "PName", "OrgType", "PostCode"], rows=INPUT_ROWS
+    )
+)
+
+MOCK_PRACTICE_RESPONSE_CONTENT = (
+    b'{"Organisations": [{"Name": "Test GP", "OrgId": "A12345"}, '
+    b'{"Name": "Test GP 2", "OrgId": "B12345"}, '
+    b'{"Name": "Test GP 3", "OrgId": "C12345"}]}'
+)
+MOCK_CCG_RESPONSE_CONTENT = (
+    b'{"Organisations": [{"Name": "Test CCG", "OrgId": "12A"}, '
+    b'{"Name": "Test CCG 2", "OrgId": "13B"}, '
+    b'{"Name": "Test CCG 3", "OrgId": "14C"}]}'
+)
+
+EXPECTED_PRACTICES = [
+    {"ods_code": "A12345", "name": "Test GP", "asids": ["000011357014"]},
+    {"ods_code": "B12345", "name": "Test GP 2", "asids": ["000022357014"]},
+    {"ods_code": "C12345", "name": "Test GP 3", "asids": ["000033357014", "123433357014"]},
+]
+
+EXPECTED_CCGS = [
+    {"ods_code": "12A", "name": "Test CCG"},
+    {"ods_code": "13B", "name": "Test CCG 2"},
+    {"ods_code": "14C", "name": "Test CCG 3"},
+]
 
 
 class ThreadedServer:
@@ -26,47 +78,58 @@ class ThreadedServer:
         self._thread.join()
 
 
+class MockOdsRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_params = parse_qs(self.path[2:])
+        primary_role = parsed_params["PrimaryRoleId"][0]
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(
+            MOCK_PRACTICE_RESPONSE_CONTENT if primary_role == "RO177" else MOCK_CCG_RESPONSE_CONTENT
+        )
+
+
 def _build_fake_s3(host, port):
     app = DomainDispatcherApplication(create_backend_app, "s3")
     server = make_server(host, port, app)
     return ThreadedServer(server)
 
 
-def gzip_file(input_file_path):
-    gzip_file_path = input_file_path.with_suffix(".gz")
-    with open(input_file_path, "rb") as input_file:
-        with gzip.open(gzip_file_path, "wb") as output_file:
-            shutil.copyfileobj(input_file, output_file)
-    return gzip_file_path
+def _build_fake_ods(host, port):
+    server = make_server(host, port, request_handler=MockOdsRequestHandler)
+    return ThreadedServer(server)
 
 
-def test_with_s3_output(datadir):
-    fake_s3_host = "127.0.0.1"
-    fake_s3_port = 8887
-    fake_s3_url = f"http://{fake_s3_host}:{fake_s3_port}"
-    fake_s3_access_key = "testing"
-    fake_s3_secret_key = "testing"
-    fake_s3_region = "us-west-1"
+def _read_s3_json_file(bucket, key):
+    f = BytesIO()
+    bucket.download_fileobj(key, f)
+    f.seek(0)
+    return json.loads(f.read().decode("utf-8"))
 
-    fake_s3 = _build_fake_s3(fake_s3_host, fake_s3_port)
+
+def test_with_s3():
+    fake_s3 = _build_fake_s3(FAKE_S3_HOST, FAKE_S3_PORT)
     fake_s3.start()
+
+    fake_ods = _build_fake_ods(FAKE_ODS_HOST, FAKE_ODS_PORT)
+    fake_ods.start()
 
     s3 = boto3.resource(
         "s3",
-        endpoint_url=fake_s3_url,
-        aws_access_key_id=fake_s3_access_key,
-        aws_secret_access_key=fake_s3_secret_key,
+        endpoint_url=FAKE_S3_URL,
+        aws_access_key_id=FAKE_S3_ACCESS_KEY,
+        aws_secret_access_key=FAKE_S3_SECRET_KEY,
         config=Config(signature_version="s3v4"),
-        region_name=fake_s3_region,
+        region_name=FAKE_S3_REGION,
     )
 
     output_bucket_name = "prm-gp2gp-ods-data"
     output_bucket = s3.Bucket(output_bucket_name)
     output_bucket.create()
 
-    input_mapping_file = gzip_file(datadir / "asid_mapping.csv")
-
-    output_bucket.upload_file(input_mapping_file.__str__(), "asidLookup.csv.gz")
+    output_bucket.upload_fileobj(INPUT_ASID_CSV, "asidLookup.csv.gz")
 
     environ["AWS_ACCESS_KEY_ID"] = "testing"
     environ["AWS_SECRET_ACCESS_KEY"] = "testing"
@@ -76,19 +139,18 @@ def test_with_s3_output(datadir):
         OdsPortalConfig(
             output_file="s3://prm-gp2gp-ods-data/output.json",
             mapping_file="s3://prm-gp2gp-ods-data/asidLookup.csv.gz",
-            s3_endpoint_url=fake_s3_url,
+            s3_endpoint_url=FAKE_S3_URL,
+            search_url=FAKE_ODS_PORTAL_URL,
         )
     )
 
-    output_bucket.download_file("output.json", "output.json")
-
-    output_file_content = open("output.json").read()
+    actual = _read_s3_json_file(output_bucket, "output.json")
 
     try:
-        assert "practices" in output_file_content
-        assert "ccg" in output_file_content
+        assert actual["practices"] == EXPECTED_PRACTICES
+        assert actual["ccgs"] == EXPECTED_CCGS
     finally:
         output_bucket.objects.all().delete()
         output_bucket.delete()
+        fake_ods.stop()
         fake_s3.stop()
-        os.remove("output.json")
