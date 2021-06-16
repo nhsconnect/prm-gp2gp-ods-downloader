f6355ede1483a53113d3088a3f6b63741c4b8a54
Dorota-TW <dorota.sobkow1@nhs.net>
2021-06-16 17:17:49 +0100

    PRMT-1961 [JG/DS] Enhance test coverage for retrieve_practices_with_asids method

--- tests/unit/domain/ods_portal/sources/test_gp2gp_organisation_metadata_service.py
+++ tests/unit/domain/ods_portal/sources/test_gp2gp_organisation_metadata_service.py
@@ -21,3 +21,54 @@ def test_returns_single_practice():
     actual = metadata_service.retrieve_practices_with_asids(asid_lookup)
 
     assert actual == expected
+
+
+def test_returns_multiple_practices():
+    mock_data_fetcher = Mock()
+    mock_data_fetcher.fetch_all_practices.return_value = [
+        OrganisationDetails(ods_code="A12345", name="GP Practice"),
+        OrganisationDetails(ods_code="B56789", name="GP Practice 2"),
+        OrganisationDetails(ods_code="C56789", name="GP Practice 3"),
+    ]
+
+    metadata_service = Gp2gpOrganisationMetadataService(data_fetcher=mock_data_fetcher)
+    asid_lookup = AsidLookup(
+        [
+            OdsAsid(ods_code="A12345", asid="123456781234"),
+            OdsAsid(ods_code="B56789", asid="443456781234"),
+            OdsAsid(ods_code="C56789", asid="773456781234"),
+        ]
+    )
+
+    expected = [
+        PracticeDetails(asids=["123456781234"], ods_code="A12345", name="GP Practice"),
+        PracticeDetails(asids=["443456781234"], ods_code="B56789", name="GP Practice 2"),
+        PracticeDetails(asids=["773456781234"], ods_code="C56789", name="GP Practice 3"),
+    ]
+    actual = metadata_service.retrieve_practices_with_asids(asid_lookup)
+
+    assert actual == expected
+
+
+def test_returns_single_practice_with_multiple_asids():
+    mock_data_fetcher = Mock()
+    mock_data_fetcher.fetch_all_practices.return_value = [
+        OrganisationDetails(name="GP Practice", ods_code="A12345")
+    ]
+
+    metadata_service = Gp2gpOrganisationMetadataService(data_fetcher=mock_data_fetcher)
+    asid_lookup = AsidLookup(
+        [
+            OdsAsid(ods_code="A12345", asid="123456781234"),
+            OdsAsid(ods_code="A12345", asid="654321234564"),
+        ]
+    )
+
+    expected = [
+        PracticeDetails(
+            asids=["123456781234", "654321234564"], ods_code="A12345", name="GP Practice"
+        )
+    ]
+    actual = metadata_service.retrieve_practices_with_asids(asid_lookup)
+
+    assert actual == expected
