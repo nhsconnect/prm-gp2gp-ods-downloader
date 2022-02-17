0ab23cf18e944bbdb91619d85fab766b81df69d0
helen.zhou <helen.zhou1@nhs.net>
2022-02-17 16:22:24 +0000

    PRMT-2542 [HZ/DS] Refactor to extract S3 uri resolver code into its own class

--- src/prmods/pipeline/ods_downloader.py
+++ src/prmods/pipeline/ods_downloader.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+from datetime import datetime
 
 import boto3
 
@@ -13,7 +14,24 @@ from prmods.domain.ods_portal.ods_portal_data_fetcher import OdsPortalDataFetche
 from prmods.pipeline.config import OdsPortalConfig
 from prmods.utils.io.s3 import S3DataManager
 
-VERSION = "v3"
+
+class OdsDownloaderS3UriResolver:
+    VERSION = "v3"
+
+    def __init__(self, asid_lookup_bucket: str, ods_metadata_bucket):
+        self._asid_lookup_bucket = asid_lookup_bucket
+        self._ods_metadata_bucket = ods_metadata_bucket
+
+    def asid_lookup(self, date_anchor: datetime) -> str:
+        date_prefix = f"{date_anchor.year}/{date_anchor.month}"
+        return f"s3://{self._asid_lookup_bucket}/{date_prefix}/asidLookup.csv.gz"
+
+    def ods_metadata(self, date_anchor: datetime) -> str:
+        date_prefix = f"{date_anchor.year}/{date_anchor.month}"
+        return (
+            f"s3://{self._ods_metadata_bucket}/{self.VERSION}/{date_prefix}"
+            f"/organisationMetadata.json"
+        )
 
 
 class OdsDownloader:
@@ -22,23 +40,18 @@ class OdsDownloader:
         self._s3_manager = S3DataManager(s3)
 
         self._config = config
-
-    def _s3_date_prefix(self) -> str:
-        return f"{self._config.date_anchor.year}/{self._config.date_anchor.month}"
+        self._uris = OdsDownloaderS3UriResolver(
+            asid_lookup_bucket=self._config.mapping_bucket,
+            ods_metadata_bucket=self._config.output_bucket,
+        )
 
     def _read_asid_lookup(self) -> AsidLookup:
-        asid_lookup_s3_path = (
-            f"s3://{self._config.mapping_bucket}/{self._s3_date_prefix()}/asidLookup.csv.gz"
-        )
+        asid_lookup_s3_path = self._uris.asid_lookup(self._config.date_anchor)
         raw_asid_lookup = self._s3_manager.read_gzip_csv(asid_lookup_s3_path)
         return AsidLookup.from_spine_directory_format(raw_asid_lookup)
 
     def _write_ods_metadata(self, organisation_metadata: OrganisationMetadata):
-        metadata_output_s3_path = (
-            f"s3://{self._config.output_bucket}/{VERSION}/{self._s3_date_prefix()}"
-            f"/organisationMetadata.json"
-        )
-
+        metadata_output_s3_path = self._uris.ods_metadata(self._config.date_anchor)
         output_metadata = {
             "date-anchor": self._config.date_anchor.isoformat(),
             "build-tag": self._config.build_tag,
